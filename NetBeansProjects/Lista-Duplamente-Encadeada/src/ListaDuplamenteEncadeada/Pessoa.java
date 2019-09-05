6dedc17367aaad0c1364bf98a43896ee213417ea
henriquemgomes <henriquemcgomess@gmail.com>
2019-09-04 21:25:09 -0300

    Criacao classe Pessoa

--- NetBeansProjects/Lista-Duplamente-Encadeada/src/ListaDuplamenteEncadeada/Pessoa.java
+++ NetBeansProjects/Lista-Duplamente-Encadeada/src/ListaDuplamenteEncadeada/Pessoa.java
@@ -11,6 +11,49 @@ package ListaDuplamenteEncadeada;
  */
 public class Pessoa {
     private int id;
+    private String nome;
+    private int idade;
+    private long telefone;
+
+    public Pessoa(int id, String nome, int idade, long telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.telefone = telefone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public long getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(long telefone) {
+        this.telefone = telefone;
+    }
+    
     
     
 }
