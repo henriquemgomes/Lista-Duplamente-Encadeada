e1fe35bad84ec1a8beceb429b2551ac2053eb52e
henriquemgomes <henriquemcgomess@gmail.com>
2019-09-12 15:26:30 -0300

    Mudanca nas variaveis, implementado Insere na Frente e Avança.

--- NetBeansProjects/Lista-Duplamente-Encadeada/src/ListaDuplamenteEncadeada/ListaDuplamenteEncadeada.java
+++ NetBeansProjects/Lista-Duplamente-Encadeada/src/ListaDuplamenteEncadeada/ListaDuplamenteEncadeada.java
@@ -15,34 +15,55 @@ public class ListaDuplamenteEncadeada {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ListaDuplamenteEncadeada lista = new ListaDuplamenteEncadeada(new Pessoa(1, "Carlos", 20, 88993322l));
+        lista.insereNaFrente(new Pessoa(2, "Jeska", 22, 99856288l));
+        lista.insereNaFrente(new Pessoa(2, "Gustavo", 22, 99875428l));
+        lista.imprimeLista();
+        lista.vaParaPrimeiro();
+        System.out.println(lista.getCursor().getPessoa().getNome());
+        lista.avanca(3);
+        System.out.println(lista.getCursor().getPessoa().getNome());
+        
     }
     
-    private Caixa primeiraCaixa;
+    /*private Caixa primeiraCaixa;
     private Pessoa fichaPessoa;
-    private Caixa segundaCaixa;
+    private Caixa segundaCaixa;*/
+    //acho q n precisa dessas ai, acho q da pra controlar melhor assim ó:
+    
+    private Caixa cabeca;
+    private Caixa cursor;
+    private Caixa ultima;
+    
     
     public ListaDuplamenteEncadeada(Pessoa fichaPessoa){
-        primeiraCaixa = null;
-        this.fichaPessoa = fichaPessoa;
-        segundaCaixa = null;
+        cabeca = new Caixa(null, fichaPessoa, null);
+        cursor = cabeca;
+        ultima = cabeca;
     }
     
     // Operações do Cursor
     
     //Henrique
     private void vaParaPrimeiro(){
-        
+        cursor = cabeca;
     }
     
     //Caroline
     private void vaParaUltimo(){
-        
+        cursor = ultima;
     }
     
     //Henrique
     private void avanca(int posicao){
-        
+        //caso a posicao seja maior q o numero de objetos restantes ele aponta para o ultimo.
+        for(int i = 0; i <= posicao; i++){
+            if(cursor.getProximo() == null){
+                break;
+            } else{
+                cursor = cursor.getProximo();
+            }
+        }
     }
     
     //Caroline
@@ -58,8 +79,10 @@ public class ListaDuplamenteEncadeada {
     // Operações na lista
     
     //Henrique
-    public void insereNaFrente(){
-        
+    public void insereNaFrente(Pessoa fichaPessoa){
+        Caixa nova = new Caixa(null, fichaPessoa, cabeca);
+        cabeca.setAnterior(nova);
+        cabeca = nova;
     }
     
     //Caroline
@@ -95,6 +118,22 @@ public class ListaDuplamenteEncadeada {
     //Caroline
     public void acessaAtual(){
         
+    }
+    
+    //apenas para testes
+    public void imprimeLista(){
+        vaParaPrimeiro();
+        while(cursor.getProximo() != null){
+            System.out.print(cursor.getPessoa().getNome()+", ");
+            cursor = cursor.getProximo();
+        }
+        System.out.println(cursor.getPessoa().getNome());
+
+    }
+    
+    //apenas para testes
+    public Caixa getCursor(){
+        return this.cursor;
     }
     
     
